f6e1282590b6d0916827aa96767c3b6cf8a1acf8
asiglez <asier.gonzalezgutierrez@iesmiguelherrero.com>
2024-10-04 17:32:53 +0200

    DWEC

--- Scripts/UD2Ejercicio8.js
+++ Scripts/UD2Ejercicio8.js
@@ -1,10 +1,21 @@
 'use strict';
 function factorial(n){ 
+    let cadena='';
     let factorial=1;
 for(let i=n;i>0;i--){
 factorial*=i;
+if(n==1){
+cadena+='1*1';
+}else{
+    if(i!=1){
+        cadena+=i+'*';
+        }else{
+            cadena+=i;
+        }
 }
-return factorial;
+}
+cadena+='='+factorial;
+return cadena;
 }
 let resultado=factorial(prompt('Introduce un numero'));
 console.log(resultado);
