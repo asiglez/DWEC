a44d6dd0e5eb870a2d872ffcd897060d813774b5
asiglez <asier.gonzalezgutierrez@iesmiguelherrero.com>
2024-10-04 15:51:33 +0200

    DWEC

--- Scripts/UD2Ejercicio11.js
+++ Scripts/UD2Ejercicio11.js
@@ -1,22 +1,23 @@
 function verificarNumeros(rangoInicio, rangoFin) {
+    if(rangoInicio<=rangoFin){
     for (let i = rangoInicio; i <= rangoFin; i++) {
         let esPrimo = true;
         if (i < 2) esPrimo = false;
-
         for (let j = 2; j <= Math.sqrt(i); j++) {
             if (i % j === 0) {
                 esPrimo = false;
                 break;
             }
         }
-
         let esMultiploDe3 = (i % 3 === 0);
         let esMultiploDe5 = (i % 5 === 0);
-
         if (esMultiploDe3 || esMultiploDe5 || esPrimo) {
             console.log(`Número: ${i} - Múltiplo de 3: ${esMultiploDe3}, Múltiplo de 5: ${esMultiploDe5}, Primo: ${esPrimo}`);
         }
     }
+}else{
+    console.log('El numero de inicio es mayor que el numero final');
+}
 }
 
 // Código auxiliar para probar la aplicación
